a278d6a4cf50d905c902abc9d52a413d61ce0004
jeyme <jeymemoon@gmail.com>
2020-03-10 16:08:40 +0900

    initial commit

---
+++ Assets/Scenes/character.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character : MonoBehaviour
+{
+    public bool ground = false;
+    float preposition;
+    float nowposition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        preposition = transform.position.y;
+        nowposition = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nowposition = transform.position.y;
+        if (Input.GetKeyDown(KeyCode.Space) && ground == true)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 8), ForceMode2D.Impulse);
+            GetComponent<Animator>().SetBool("jump", true);
+            GetComponent<Animator>().SetBool("ground", false);
+            ground = false;
+        }
+        if (nowposition<preposition&& GetComponent<Animator>().GetBool("ground") == false)
+        {
+            GetComponent<Animator>().SetBool("jumpdown", true);
+        }
+        preposition = nowposition;
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.name == "ground")
+        {
+            ground = true;
+            GetComponent<Animator>().SetBool("ground", true);
+            GetComponent<Animator>().SetBool("jump", false);
+            GetComponent<Animator>().SetBool("jumpdown", false);
+        }
+    }
+}
---
+++ Assets/background.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-3 * Time.deltaTime, 0, 0);
+        if (transform.position.x < -19.2f)
+        {
+            transform.Translate(19.2f, 0, 0);
+        }
+    }
+}
